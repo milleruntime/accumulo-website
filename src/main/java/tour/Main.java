c934c56c5fd1428bf56222ee553a8e0b07293838
Mike Miller <mmiller@apache.org>
2017-11-07 18:07:53 -0500

    code

--- src/main/java/tour/Main.java
+++ src/main/java/tour/Main.java
@@ -6,11 +6,14 @@ import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.BatchWriterConfig;
 import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
+import org.apache.accumulo.core.security.ColumnVisibility;
+import org.apache.accumulo.core.security.TablePermission;
 import org.apache.accumulo.minicluster.MiniAccumuloCluster;
 
 import java.nio.file.Files;
@@ -32,37 +35,47 @@ public class Main {
 
     private static void exercise(MiniAccumuloCluster mac) throws Exception{
         // start writing your code here
-        // 1. Connect to Mini Accumulo as the root user and create a table called "GothamPD".
+        // Connect to Mini Accumulo as the root user and create a table called "GothamPD".
         Connector conn = mac.getConnector("root", "tourguide");
         conn.tableOperations().create("GothamPD");
 
-        // 2. Create a Mutation object to hold all changes to a row in a table.  Each row has a unique row ID.
+        // Create a "secretIdentity" authorization & visibility
+        final String secId = "secretIdentity";
+        Authorizations auths = new Authorizations(secId);
+        ColumnVisibility visibility = new ColumnVisibility(secId);
+
+        // Create a user with the "secretIdentity" authorization and grant him read permissions on our table
+        conn.securityOperations().createLocalUser("commissioner", new PasswordToken("gordanrocks"));
+        conn.securityOperations().changeUserAuthorizations("commissioner", auths);
+        conn.securityOperations().grantTablePermission("commissioner", "GothamPD", TablePermission.READ);
+
+        // Create 3 Mutation objects to hold each person of interest.
         Mutation mutation1 = new Mutation("id0001");
         Mutation mutation2 = new Mutation("id0002");
         Mutation mutation3 = new Mutation("id0003");
 
-        // 3. Create key/value pairs for Batman.  Put them in the "hero" family.
+        // Create key/value pairs for each Mutation, putting them in the appropriate family.
         mutation1.put("hero","alias", "Batman");
-        mutation1.put("hero","name", "Bruce Wayne");
+        mutation1.put("hero","name", visibility,"Bruce Wayne");
         mutation1.put("hero","wearsCape?", "true");
         mutation2.put("hero","alias", "Robin");
-        mutation2.put("hero","name", "Dick Grayson");
+        mutation2.put("hero","name", visibility,"Dick Grayson");
         mutation2.put("hero","wearsCape?", "true");
         mutation3.put("villain","alias", "Joker");
         mutation3.put("villain","name", "Unknown");
         mutation3.put("villain","wearsCape?", "false");
 
-        // 4. Create a BatchWriter to the GothamPD table and add your mutation to it.  Try w/ resources will close for us.
+        // Create a BatchWriter to the GothamPD table and add your mutations to it.  Try w/ resources will close for us.
         try(BatchWriter writer = conn.createBatchWriter("GothamPD", new BatchWriterConfig())) {
             writer.addMutation(mutation1);
             writer.addMutation(mutation2);
             writer.addMutation(mutation3);
         }
 
-        // 5. Read and print all rows of the "GothamPD" table. Try w/ resources will close for us.
-        try(Scanner scan = conn.createScanner("GothamPD", Authorizations.EMPTY)) {
+        // Read and print all rows of the commissioner can see. Pass Scanner proper authorizations
+        Connector commishConn = mac.getConnector("commissioner", "gordanrocks");
+        try(Scanner scan = commishConn.createScanner("GothamPD", auths)) {
             System.out.println("Gotham Police Department Persons of Interest:");
-            // A Scanner is an extension of java.lang.Iterable so behaves just like one.
             for (Map.Entry<Key, Value> entry : scan) {
                 System.out.println("Key:" + entry.getKey());
                 System.out.println("Value:" + entry.getValue());
