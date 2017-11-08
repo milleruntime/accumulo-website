6f8338787ce351a17aaf31a7cbdddcf5796ae436
Mike Miller <mmiller@apache.org>
2017-11-08 18:25:04 -0500

    code

--- src/main/java/tour/Main.java
+++ src/main/java/tour/Main.java
@@ -1,13 +1,18 @@
 package tour;
 
 // Classes you will use along the tour
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import org.apache.accumulo.core.client.BatchScanner;
 import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.BatchWriterConfig;
 import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+import org.apache.accumulo.core.data.Column;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Range;
@@ -20,6 +25,7 @@ import org.apache.accumulo.minicluster.MiniAccumuloCluster;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.apache.hadoop.io.Text;
 
 public class Main {
 
@@ -40,29 +46,36 @@ public class Main {
         Connector conn = mac.getConnector("root", "tourguide");
         conn.tableOperations().create("GothamPD");
 
-        //
+        // Generate 10,000 rows of henchman data
         try(BatchWriter writer = conn.createBatchWriter("GothamPD", new BatchWriterConfig())) {
             for(int i = 0; i < 10_000; i++) {
                 Mutation m = new Mutation(String.format("id%04d", i));
-                String cf = String.format("henchmanType%d", (i % 5));
-                m.put(cf, "alias", "henchman" + i);
-                m.put(cf, "yearsOfService", "" + (new Random().nextInt(50)));
-                m.put(cf, "wearsCape?", "false");
+                m.put("villain", "alias", "henchman" + i);
+                m.put("villain", "yearsOfService", "" + (new Random().nextInt(50)));
+                m.put("villain", "wearsCape?", "false");
                 writer.addMutation(m);
             }
         }
 
-        //
-        try(BatchScanner scan = conn.createBatchScanner("GothamPD", Authorizations.EMPTY, 2) {
-            scan.setRange(new Range("id6999", "id7001"));
-            System.out.println("Gotham Police Department Persons of Interest:");
-            Long read = 0L;
-            Long start = System.nanoTime();
-            for (Map.Entry<Key, Value> entry : scan) {
-                System.out.println("Key:" + entry.getKey());
-                System.out.println("Value:" + entry.getValue());
+        // 1. Create a BatchScanner with 5 query threads
+        try(BatchScanner batchScanner = conn.createBatchScanner("GothamPD", Authorizations.EMPTY, 5)) {
+            // 2. Create a collection of 2 sample ranges and set it to the batchScanner
+            List ranges = new ArrayList<Range>();
+            ranges.add(new Range("id1000", "id1999"));
+            ranges.add(new Range("id9000", "id9999"));
+            batchScanner.setRanges(ranges);
+
+            // 3. Fetch just the columns we want
+            batchScanner.fetchColumn(new Text("villain"), new Text("yearsOfService"));
+
+            // 4. Calculate average years of service
+            Long totalYears = 0L;
+            Long entriesRead = 0L;
+            for (Map.Entry<Key, Value> entry : batchScanner) {
+                totalYears += Long.valueOf(entry.getValue().toString());
+                entriesRead++;
             }
-            System.out.println("Range took " + (System.nanoTime() - start)/1000.0 + " to read " + read);
+            System.out.println("Out of " + entriesRead + " entries, average years of a henchman: " + totalYears / entriesRead);
         }
     }
 }
